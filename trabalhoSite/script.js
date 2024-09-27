48e596ad78aa9196f22172833fe0213a8643b839
Luiz Alves <115586109+luizz-dev@users.noreply.github.com>
2024-09-26 22:26:38 -0300

    Add files via upload

---
+++ trabalhoSite/script.js
@@ -0,0 +1,7 @@
+const linkDados = document.getElementById("conferir-dado");
+
+linkDados.addEventListener("click", function () {
+  window.location.assign(
+    "https://ge.globo.com/futebol/times/sao-paulo/noticia/2024/02/04/campeao-de-tudo-e-mais-um-pouco-veja-como-fica-a-lista-de-titulos-do-sao-paulo.ghtml"
+  );
+});
